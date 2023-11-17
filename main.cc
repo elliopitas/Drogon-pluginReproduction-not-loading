efa39afa51b6fea021c2087af801985a6c2fa4b1
George Constantinides <George007005001002@gmail.com>
2023-11-17 00:51:36 +0000

    YAML

--- main.cc
+++ main.cc
@@ -1,8 +1,8 @@
 #include <drogon/drogon.h>
 int main() {
     //Load config file
-    drogon::app().loadConfigFile("../config.json");
-//    drogon::app().loadConfigFile("../config.yaml");
+//    drogon::app().loadConfigFile("../config.json");
+    drogon::app().loadConfigFile("../config.yaml");
     //Run HTTP framework,the method will block in the internal event loop
 	LOG_INFO<<"Starting Drogon";
     drogon::app().run();
